2934edbe555e8663ebf166f28fc6be2eba464691
Bogdan Floris <bogdan.floris@gmail.com>
2020-02-06 16:28:26 +0100

    updated run

--- run.py
+++ run.py
@@ -4,4 +4,7 @@ from streams.drift_experiments import drift_detection_different_embeddings
 if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print("Running on {}".format(device))
-    drift_detection_different_embeddings(batch_size=32, device=device)
+    try:
+        drift_detection_different_embeddings(batch_size=32, device=device)
+    except Exception as e:
+        print(e)
