db3c9fcd4155adc1e459c9fabcc098cfaddb4ad1
Bogdan Floris <bogdan.floris@gmail.com>
2020-02-06 14:29:03 +0100

    feat/job

--- job.sh
+++ job.sh
@@ -11,10 +11,10 @@ module purge
 module load 2019
 module load Python/3.7.5-foss-2018b
 
-cp -r "$HOME"/detecting-and-addressing-change/ "$TMPDIR"/
+cp -r "$HOME"/detecting-and-addressing-change/ "$TMPDIR"/detecting-and-addresing-change/
 cd "$TMPDIR"/detecting-and-addressing-change/ || return
 
 RUN_NAME="$(date +%d_%m_%Y_%H_%M_%S)";
 python3 run.py >> "output_${RUN_NAME}.txt"
 
-rsync -a ./ "$HOME"/detecting-and-addressing-change/
+rsync -a ./detecting-and-addresing-change/ "$HOME"/detecting-and-addressing-change/
