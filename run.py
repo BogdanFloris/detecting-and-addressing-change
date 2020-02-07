d1058f5938de88b73a7a7dd7bff8a5e3754a5caa
Bogdan Floris <bogdan.floris@gmail.com>
2020-02-07 15:27:16 +0100

    fix: run script

--- run.py
+++ run.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     print("Running on {}".format(device))
     try:
         # drift_detection_different_embeddings(batch_size=32, device=device)
-        train_wos_batch(epochs=5, device=device)
+        train_wos_batch(epochs=3, transform=True, device=device)
     except Exception as e:
         print(e)
