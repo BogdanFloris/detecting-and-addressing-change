675a34e13b01e5e849ec36cecdd765513f7288a2
Bogdan Floris <bogdan.floris@gmail.com>
2020-02-07 12:47:28 +0100

    fix: job script and run file

--- job.sh
+++ job.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-# SBATCH --job-name=conceptdrift
-# SBATCH --nodes=1
-# SBATCH --time=1:00:00
-# SBATCH --ntasks-per-node=1
-# SBATCH --mem=16G
-# SBATCH --mail-type=END
-# SBATCH --mail-user=bogdan.floris@gmail.com
+#SBATCH --job-name=conceptdrift
+#SBATCH --nodes=1
+#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu_short
+#SBATCH --mem=16G
+#SBATCH --mail-type=END
+#SBATCH --mail-user=bogdan.floris@gmail.com
 
 module purge
 module load 2019
+module load CUDA/10.1.243
 module load Python/3.7.5-foss-2018b
 
-cp -r "$HOME"/detecting-and-addressing-change/ "$TMPDIR"/detecting-and-addresing-change/
-cd "$TMPDIR"/detecting-and-addressing-change/ || return
+cp -r "$HOME"/detecting-and-addressing-change/ "$TMPDIR"
+# shellcheck disable=SC2164
+cd "$TMPDIR"/detecting-and-addressing-change/
 
 RUN_NAME="$(date +%d_%m_%Y_%H_%M_%S)";
 python3 run.py >> "output_${RUN_NAME}.txt"
--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import torch
 from streams.drift_experiments import drift_detection_different_embeddings
+from models.wos_train import train_wos_batch
 
 if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print("Running on {}".format(device))
     try:
-        drift_detection_different_embeddings(batch_size=32, device=device)
+        # drift_detection_different_embeddings(batch_size=32, device=device)
+        train_wos_batch(epochs=5, device=device)
     except Exception as e:
         print(e)
